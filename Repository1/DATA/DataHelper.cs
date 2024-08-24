c3f2d79d6fb38c2aa75ca1734578f7d0467dbc9c
FlorC <412289@tecnicatura.frc.utn.edu.ar>
2024-08-23 21:31:52 -0300

    Capas y patrones

---
+++ Repository1/DATA/DataHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository1.DATA
+{
+    public class DataHelper                   //PATRÓN SINGLETON: para tener una sola instancia de esta clase.
+                                              //Es importante mantener privada la conexión a la BD, para que solo se conecte desde un solo lugar. 
+                                              //DATA HELPER ES LA CAPA DONDE GUARDO TODA LA COMUNICACIÓN CON LA BD (ex ADO)
+    {
+        
+         
+        private static DataHelper instance;
+        private SqlConnection cnn;
+
+        private  DataHelper()
+        {
+            cnn = new SqlConnection(Properties.Resources.strConnect); 
+        }
+
+        public static DataHelper GetInstance()
+        {
+            if(instance == null)                    //si aún no fue creada, la crea
+            {
+                instance = new DataHelper();
+            }
+            return instance;                       //si ya fue creada, devuelve la existente
+        }
+
+        private void Connect()
+        {
+            
+        }
+
+
+        public DataTable ExecuteSPQuery(string sp)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                //this.Connect();
+                cnn.Open();
+                var cmd = new SqlCommand(sp, cnn);
+                cmd.CommandType = CommandType.StoredProcedure;        //Tengo que dejar indicado que lo que le voy a pasar es un SP y no una query
+
+                dt.Load(cmd.ExecuteReader());
+
+                cnn.Close();
+            }
+            catch (SqlException)                       //CONTROLA POSIBLE ERROR EN TIEMPO DE EJECUCIÓN
+            {
+
+                throw;
+            }
+            
+
+            return dt;
+        }
+    }
+}
---
+++ Repository1/DATA/ProdRepositoryADO.cs
@@ -0,0 +1,51 @@
+﻿using Repository1.DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Repository1.DATA
+{
+    public class ProdRepositoryADO : iRepository
+    {
+        
+       
+
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            List<Product> list = new List<Product>();
+            var dt = DataHelper.GetInstance().ExecuteSPQuery("SP_recuperar");  //INVOCA AL MÉTODO ESTÁTICO DE LA CLASE DATAHELPER, LUEGO AL MÉTODO QUE CONECTA Y HACE LA CONSULTA Y MANDA LA QUERY CON FORMA DE sp
+            foreach (DataRow dr in dt.Rows)
+            {
+                Product p = new Product();                      //Por cada fila que trae el método, creo un objeto y lo mapeo: cargo sus properties con el contenido de cada columna
+                p.Codigo = Convert.ToInt32(dr[0]);
+                p.Nombre = Convert.ToString(dr[1]);
+                p.Precio = Convert.ToDouble(dr[2]);
+                p.Stock = Convert.ToInt32(dr[3]);
+                p.Activo = Convert.ToBoolean(dr[4]);
+                list.Add(p);
+            }   
+            return list;
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
